6d3e423fb5c7ec23aabfdd6fd34bbe98505b574e
Weren Silva <weren.silva@dtidigital.com.br>
2022-09-21 18:43:47 -0300

    add inputs

--- scripts.js
+++ scripts.js
@@ -1,14 +1,31 @@
-let components = ['addButtonContent', 'categoryButtonsContent', 'blank'];
+let components = ['addButtonContent', 'categoryButtonsContent', 'blank', 'budgetInputContent'];
 
-const handleShowButtons = (component) => {
+const handleShowContent = (component) => {
     components.forEach((c) => {
         document.querySelector('#' + c).classList.add('hidden');
     });
     document.querySelector('#' + component).classList.remove('hidden');
 };
 
+const handleAddBudget = (budgetType) => {
+console.log(budgetType)
+}
+
 onload = () => {
     document.querySelector('#addButton').onclick = (e) => {
-        handleShowButtons('categoryButtonsContent');
+        handleShowContent('categoryButtonsContent');
+    }
+
+    document.querySelector('#travelerButton').onclick = (e) => {
+        handleShowContent('budgetInputContent');
+        handleAddBudget('traveler');
+    }
+    document.querySelector('#busButton').onclick = (e) => {
+        handleShowContent('budgetInputContent');
+        handleAddBudget('bus');
+    }
+    document.querySelector('#carButton').onclick = (e) => {
+        handleShowContent('budgetInputContent');
+        handleAddBudget('car');
     }
 }
